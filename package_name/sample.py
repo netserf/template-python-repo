4b554480bab5e27ea5fa35e2df8e9cf7e829c9a5
netserf <networkserf@gmail.com>
2022-12-08 16:24:08 -0800

    fix: template cleanup

---
+++ package_name/sample.py
@@ -0,0 +1,7 @@
+"""Sample module docstring
+"""
+
+
+def sample_function():
+    """Sample function docstring"""
+    return True
--- tests/test_sample.py
+++ tests/test_sample.py
@@ -1,5 +1,13 @@
-# Sample Test passing with nose and pytest
+"""Sample test module for package_name"""
+
+from package_name import sample
 
 
 def test_pass():
+    """Sample test function"""
     assert True, "dummy sample test"
+
+
+def test_sample_function():
+    """Sample test function using package_name.sample.sample_function"""
+    assert sample.sample_function(), "sample function should return True"
